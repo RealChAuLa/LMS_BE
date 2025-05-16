380a5fbc3af984260d28fc6f5723b78bde385423
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-05-16 23:40:57 +0530

    Initial commit

---
+++ src/main/java/com/lms/API/LmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.lms.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsBeApplication.class, args);
+    }
+
+}
